413683e9e959d9f5ed73c99a485cc0c2f0a00d2e
ashvin <ashvinanilkumarsh@gmail.com>
2025-06-05 08:09:31 +0530

    Add basic workflow.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{env};
+use std::fs; // For reading the file
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+
+    println!("Searching for {:?}",query);
+    println!("In file {:?}",filename);
+
+    let contents = fs::read_to_string(filename).
+    expect("File does not exist.");
+    
+    println!("{:?}",contents)
 }
